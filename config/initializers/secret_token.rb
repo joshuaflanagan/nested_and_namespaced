ff77f64a9fd7abe9ce44209484b2b4934a42a36a
Joshua Flanagan <joshuaflanagan@gmail.com>
2011-12-05 19:03:04 -0600

    rails new nested

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nested::Application.config.secret_token = '71f0f155e8ee9cd5f9f172a04a917223e925996db873b199b63b9f73feb383aa53375a90d044fd616573aaa1377cde9a9a45c421e593695cd8be285dfd385644'
